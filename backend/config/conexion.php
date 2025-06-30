e228939a5cad6d841489057150b11be66f3a2f71
YolotlTellez29 <12yolotl56@gmail.com>
2025-06-30 00:41:13 -0600

    creandoenv

--- backend/config/conexion.php
+++ backend/config/conexion.php
@@ -1,8 +1,13 @@
 <?php
-$host = "localhost";
-$db = "mariscos_db";
-$user = "usuario";
-$pass = "contraseña";
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$db   = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
